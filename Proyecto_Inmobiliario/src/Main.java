e9cae00536e0c0be9de89063fce794a677b7041b
hakasaxx <hakasax@tutamail.com>
2026-09-07 11:33:53 -0300

    mish

---
+++ Proyecto_Inmobiliario/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    this.testTasarDepto();
+}
+void testTasarDepto() {
+    System.out.println("\n --- test tasacion depto ---");
+    Departamento depto = new Departamento(new Propietario("Jose", 23, 'M', false));
+    new Direccion("Alameda", 232, "Santiago");
+    //Arreglar cuando el prfe suba la wea
+}
